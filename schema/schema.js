e9483c876bc4d9c736f3863a8501e195f9f656d0
Taylor Artemenko <taylorartemenko6@gmail.com>
2020-11-26 22:23:48 -0800

    Environment Variable Configuration

--- schema/schema.js
+++ schema/schema.js
@@ -4,12 +4,11 @@ const moment = require('moment');
 const Note = require('../models/note');
 const User = require('../models/user');
 const Upcoming = require('../models/upcoming');
-const cloudConf = require('../conf/cloudConf.js');
 
 cloudinary.config({
-  cloud_name: cloudConf.cloud_name,
-  api_key: cloudConf.api_key,
-  api_secret: cloudConf.api_secret
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET
 });
 
 const {
--- src/server.js
+++ src/server.js
@@ -3,14 +3,12 @@ const bodyParser = require('body-parser');
 const { graphqlHTTP } = require('express-graphql');
 const { graphqlUploadExpress } = require('graphql-upload');
 const mongoose = require('mongoose');
-const mongooseConf = require('../conf/mongooseConn.js');
 const schema = require('../schema/schema');
 const checkUpcoming = require('../checkUpcoming.js');
 const serveless = require('serverless-http');
 
 // Server setup
 const app = express();
-const port = process.env.PORT || 4000;
 
 const router = express.Router();
 
@@ -18,7 +16,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Database connection
-const conn = mongooseConf.conn;
+const conn = process.env.MONGODB_URI;
 mongoose.connect(conn, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.connection.once('open', () => {
   console.log('connected to MongoDB database');
