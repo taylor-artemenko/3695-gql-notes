b14d59c7d3eb930225cf48c6d0e67d2e20911850
Taylor Artemenko <taylorartemenko6@gmail.com>
2020-11-26 22:30:47 -0800

    .gitignore fix

--- conf/cloudConf.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  cloud_name: 'tartemenko',
-  api_key: '876167252683444',
-  api_secret: 'Qbp1htJlYsyWuaTl97qUfDIqZZ4'
-}
--- conf/mongooseConn.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  conn: 'mongodb+srv://dbadmin:p4ssw0rd@graphql-api.a6trp.mongodb.net/graphqlNotes?retryWrites=true&w=majority'
-}
