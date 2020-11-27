f318b04743ce1aed473ed105e3d45c16db638690
Taylor Artemenko <taylorartemenko6@gmail.com>
2020-11-26 22:07:54 -0800

    Refactor for Netlify Deployment with Serverless functions

---
+++ conf/cloudConf.js
@@ -0,0 +1,5 @@
+module.exports = {
+  cloud_name: 'tartemenko',
+  api_key: '876167252683444',
+  api_secret: 'Qbp1htJlYsyWuaTl97qUfDIqZZ4'
+}
---
+++ conf/mongooseConn.js
@@ -0,0 +1,3 @@
+module.exports = {
+  conn: 'mongodb+srv://dbadmin:p4ssw0rd@graphql-api.a6trp.mongodb.net/graphqlNotes?retryWrites=true&w=majority'
+}
